fab2f20d530638f85a6afe1bf865b38fcb5aa8b0
sergioalcazar745 <sergioalcazar745@gmail.com>
2023-01-29 23:58:10 +0100

    29/01/2023

---
+++ AdoNet/Form07CRUD.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace AdoNet
+{
+    partial class Form07CRUD
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpHospital = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNumCama = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHospitalCod = new System.Windows.Forms.TextBox();
+            this.grdHospital = new System.Windows.Forms.DataGridView();
+            this.columnHospitalCod = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnNumCama = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.grpHospital.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdHospital)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grpHospital
+            // 
+            this.grpHospital.Controls.Add(this.button1);
+            this.grpHospital.Controls.Add(this.label5);
+            this.grpHospital.Controls.Add(this.txtNumCama);
+            this.grpHospital.Controls.Add(this.label4);
+            this.grpHospital.Controls.Add(this.txtTelefono);
+            this.grpHospital.Controls.Add(this.label3);
+            this.grpHospital.Controls.Add(this.txtDireccion);
+            this.grpHospital.Controls.Add(this.label2);
+            this.grpHospital.Controls.Add(this.txtNombre);
+            this.grpHospital.Controls.Add(this.label1);
+            this.grpHospital.Controls.Add(this.txtHospitalCod);
+            this.grpHospital.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.grpHospital.Location = new System.Drawing.Point(611, 34);
+            this.grpHospital.Name = "grpHospital";
+            this.grpHospital.Size = new System.Drawing.Size(570, 184);
+            this.grpHospital.TabIndex = 1;
+            this.grpHospital.TabStop = false;
+            this.grpHospital.Text = "Hospital";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(393, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 39);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Insertar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(211, 103);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 21);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Num_Cama";
+            // 
+            // txtNumCama
+            // 
+            this.txtNumCama.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtNumCama.Location = new System.Drawing.Point(211, 127);
+            this.txtNumCama.Name = "txtNumCama";
+            this.txtNumCama.Size = new System.Drawing.Size(159, 29);
+            this.txtNumCama.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(26, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 21);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Telefono";
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtTelefono.Location = new System.Drawing.Point(26, 127);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(159, 29);
+            this.txtTelefono.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(393, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 21);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Direccion";
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtDireccion.Location = new System.Drawing.Point(393, 56);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(159, 29);
+            this.txtDireccion.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(211, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtNombre.Location = new System.Drawing.Point(211, 56);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(159, 29);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(26, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hospital_Cod";
+            // 
+            // txtHospitalCod
+            // 
+            this.txtHospitalCod.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtHospitalCod.Location = new System.Drawing.Point(26, 56);
+            this.txtHospitalCod.Name = "txtHospitalCod";
+            this.txtHospitalCod.Size = new System.Drawing.Size(159, 29);
+            this.txtHospitalCod.TabIndex = 0;
+            // 
+            // grdHospital
+            // 
+            this.grdHospital.AllowUserToAddRows = false;
+            this.grdHospital.BackgroundColor = System.Drawing.Color.White;
+            this.grdHospital.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdHospital.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.columnHospitalCod,
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.columnNumCama});
+            this.grdHospital.Location = new System.Drawing.Point(35, 34);
+            this.grdHospital.Name = "grdHospital";
+            this.grdHospital.RowTemplate.Height = 25;
+            this.grdHospital.Size = new System.Drawing.Size(543, 190);
+            this.grdHospital.TabIndex = 2;
+            // 
+            // columnHospitalCod
+            // 
+            this.columnHospitalCod.HeaderText = "Hospital_Cod";
+            this.columnHospitalCod.Name = "columnHospitalCod";
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "Nombre";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "Direccion";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.HeaderText = "Telefono";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // columnNumCama
+            // 
+            this.columnNumCama.HeaderText = "Num_Cama";
+            this.columnNumCama.Name = "columnNumCama";
+            // 
+            // Form07CRUD
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1208, 450);
+            this.Controls.Add(this.grdHospital);
+            this.Controls.Add(this.grpHospital);
+            this.Name = "Form07CRUD";
+            this.Text = "Form07CRUD";
+            this.grpHospital.ResumeLayout(false);
+            this.grpHospital.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdHospital)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private GroupBox grpHospital;
+        private Label label1;
+        private TextBox txtHospitalCod;
+        private Label label5;
+        private TextBox txtNumCama;
+        private Label label4;
+        private TextBox txtTelefono;
+        private Label label3;
+        private TextBox txtDireccion;
+        private Label label2;
+        private TextBox txtNombre;
+        private Button button1;
+        private DataGridView grdHospital;
+        private DataGridViewTextBoxColumn columnHospitalCod;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private DataGridViewTextBoxColumn columnNumCama;
+    }
+}
---
+++ AdoNet/Models/Hospital.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNet.Models
+{
+    public class Hospital
+    {
+        public Hospital() 
+        {
+        
+        }
+
+        #region PROPIEDADES
+        public int Hopistal_Cod {get; set;}
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public int Telefono { get; set; }
+        public int Num_Cama { get; set; }
+        #endregion
+    }
+}
